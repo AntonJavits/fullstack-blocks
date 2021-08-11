2b0180ba1d87fb36bb858e927d99a28e01632d3c
AntonJ <anton.javits@gmail.com>
2021-08-11 23:28:15 +0300

    select single from the list, weather integration

--- part2/maiden-tiedot/src/components/CountryList.js
+++ part2/maiden-tiedot/src/components/CountryList.js
@@ -1,11 +1,30 @@
+import { useState } from 'react'
+import SingleCountry from './SingleCountry'
+
 const CountryList = ( {filteredCountries} ) => {
-  return(
-    <ul>
-      {filteredCountries.map(country => {
-        return <li key={country.alpha3code}>{country.name}</li>
-      })}
-    </ul>
-  )
+  const [indexOfSelected, setIndexOfSelected] = useState('')
+  
+  const showOne = (index) => {
+    setIndexOfSelected(index)
+  }
+
+  if (indexOfSelected === '') {
+    return(
+      <ul>
+        {filteredCountries.map((country, index) => {
+          return(
+            <li key={country.alpha3code}>
+              {country.name} <button onClick={() => showOne(index)}>Show</button>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  } else {
+    return (
+      <SingleCountry country={filteredCountries[indexOfSelected]} />
+    )
+  }
 }
 
 export default CountryList
--- part2/maiden-tiedot/src/components/SingleCountry.js
+++ part2/maiden-tiedot/src/components/SingleCountry.js
@@ -1,17 +1,52 @@
+// http://api.weatherstack.com/
+
+import axios from 'axios'
+import React, {useState, useEffect}  from 'react';
+
 const SingleCountry = ( {country} ) => {
-  console.log(country)
+  const api_key = process.env.REACT_APP_API_KEY  
+  console.log('api', api_key);
+  
+  const [weather, setWeather] = useState({
+    temperature: '',
+    wind: '',
+    wind_dir: '',
+    icon: ''
+  })
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}&units=m`)
+      .then(response => {
+        let weatherData = {
+          temperature: response.data.current.temperature,
+          wind: response.data.current.wind_speed,
+          wind_dir: response.data.current.wind_dir,
+          icon: response.data.current.weather_icons[0]
+        }
+        setWeather(weatherData)
+      })
+  }, [api_key, country.capital])
+
   return(
     <>
-    <h2>{country.name}</h2>
-    <p>
-      Capital: {country.capital}<br />
-      Population: {country.population}
-    </p>
-    <h3>Languages</h3>
-    <ul>
-        {country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}    
-    </ul>
-    <img src={country.flag} height="100" alt="Flag"/>
+      <h2>{country.name}</h2>
+      <p>
+        Capital: {country.capital}<br />
+        Population: {country.population}
+      </p>
+      <h3>Languages</h3>
+      <ul>
+          {country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}    
+      </ul>
+      <img src={country.flag} height="100" alt="Flag"/>
+      
+      <h3>Weather</h3>
+      <ul>
+        <li>temperature: {weather.temperature} Celsius</li>
+        <li>wind: {weather.wind} m/s, direction {weather.wind_dir}</li>
+        <li><img src={weather.icon} alt="weather icon" /></li>
+      </ul>
     </>
   )
 }
